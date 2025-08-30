233f5f204734aeaeca517393af263f1adde09a31
Alex <learnhtml1921@gmail.com>
2025-08-30 21:57:02 +0530

    Create twoNumadd.java

---
+++ twoNumadd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Demo {
+	public static void main(String[] args) {
+		int a;
+		int b;
+		int c;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter first number: ");
+		a = sc.nextInt();
+		System.out.print("Enter second number: ");
+		b = sc.nextInt();
+		System.out.println("Output :");
+		if (a != b) {
+			c = (a+b) * 2;
+			System.out.println(c);
+		}
+		else {
+			c = a+b;
+			System.out.println(c);
+		}
+	}
+}
