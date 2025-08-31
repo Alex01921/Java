faef60e84bb8216d71eda18d6fdbdd8b19acf14b
Alex <learnhtml1921@gmail.com>
2025-08-31 17:15:17 +0530

    Create Assignment18pattern.java

---
+++ Assignment18pattern.java
@@ -0,0 +1,22 @@
+public class Demo {
+    public static void main(String[] args) {
+        int rows = 5, i, j;
+        for(i = 5; i <= rows; i--) {
+            for(j = 1; j <= i;j++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
+/*
+
+Output: 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+*/
